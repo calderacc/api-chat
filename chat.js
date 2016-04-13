8aab41912bfd38aca079ac220966ccb566858435
Malte Hübner <maltehuebner@gmx.org>
2016-04-14 00:08:30 +0200

    More WIP.

--- chat.js
+++ chat.js
@@ -1,6 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var mysqlConnectionPool = require('../api-commons/connectionPool.js').mysqlConnectionPool;
+
+var configFile = fs.readFileSync('../api-commons/config.json');
+var config;
+
+try {
+    config = JSON.parse(configFile);
+} catch (err) {
+    console.log('There has been an error parsing your JSON.');
+    console.log(err);
+}
 
 app.get('/', function(req, res){
     res.send('<h1>Hello world</h1>');
@@ -11,7 +23,34 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+    socket.on('message', function(message) {
+        message.timestamp = Date.now();
+        message.username = 'maltehuebner';
+        message.userColor = 'rgb(255, 255, 0)';
+
+        io.emit('message', message);
+        saveMessageToDatabase(message);
     });
 });
+
+function saveMessageToDatabase(message) {
+    var query = 'INSERT INTO post SET user_id = (SELECT id FROM fos_user_user WHERE token = \'' + message.userToken + '\'), message = \'' + message.message + '\', dateTime = NOW(), enabled = 1, chat = 1;';
+
+    runDatabaseQuery(query, null);
+
+    console.log(query);
+}
+
+function runDatabaseQuery(queryString, callbackFunction) {
+    mysqlConnectionPool.getConnection(function(err, connection) {
+        connection.query('USE ' + config.database.dbname + ';', function(err, rows) {
+            connection.query(queryString, function(err, rows) {
+                connection.release();
+
+                if (callbackFunction) {
+                    callbackFunction(rows);
+                }
+            });
+        });
+    });
+}
